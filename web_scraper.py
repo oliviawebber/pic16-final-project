316b75e880cf3130b3973ab64a2d1c583785b65e
Layton Webber <webberlh16@gmail.com>
2019-03-02 12:20:59 -0800

    Added pitcher data.

--- web_scraper.py
+++ web_scraper.py
@@ -9,13 +9,9 @@ from bs4 import BeautifulSoup
 import csv
 
 
-#url = "https://www.spotrac.com/mlb/statistics/player/#name=&type=yearly&pctGames=50&season=2012%2C2018&conference=&team=&positions%5B%5D=74&positions%5B%5D=34&positions%5B%5D=61&positions%5B%5D=35&positions%5B%5D=36&positions%5B%5D=37&positions%5B%5D=38&positions%5B%5D=62&positions%5B%5D=41&positions%5B%5D=40&positions%5B%5D=39&positions%5B%5D=42&salary=545000%2C34083333&status%5B%5D=active&sort=17%2C1"
-
-#raw_html = requests.get(url)
-
-with open('stats.csv','w') as csvfile:
+with open('batter_stats.csv','w') as csvfile:
     writer = csv.writer(csvfile)
-    raw_html = open('stats.html').read()
+    raw_html = open('batter_stats.html').read()
     html = BeautifulSoup(raw_html, 'html.parser')
     
     table_headers = html.table.thead
@@ -25,6 +21,7 @@ with open('stats.csv','w') as csvfile:
     for cell in table_headers.tr.select('th'):
         raw_text = str(cell.text)
         col_headers.append(raw_text)
+    col_headers.insert(2, "Position")
     writer.writerow(col_headers)
     
     for row in table_data.select('tr'):
@@ -32,5 +29,34 @@ with open('stats.csv','w') as csvfile:
         for cell in row.select('td'):
             raw_text = str(cell.text)
             row_data.append(raw_text)
+        player_and_pos = row_data[1]
+        split = player_and_pos.rsplit('.')
+        row_data[1] = split[0]
+        row_data.insert(2, split[1][1:])
         writer.writerow(row_data)
     
+with open('pitcher_stats.csv','w') as csvfile:
+    writer = csv.writer(csvfile)
+    raw_html = open('pitcher_stats.html').read()
+    html = BeautifulSoup(raw_html, 'html.parser')
+    
+    table_headers = html.table.thead
+    table_data = html.table.tbody
+    
+    col_headers = []
+    for cell in table_headers.tr.select('th'):
+        raw_text = str(cell.text)
+        col_headers.append(raw_text)
+    col_headers.insert(2, "Position")
+    writer.writerow(col_headers)
+    
+    for row in table_data.select('tr'):
+        row_data = []
+        for cell in row.select('td'):
+            raw_text = str(cell.text)
+            row_data.append(raw_text)
+        player_and_pos = row_data[1]
+        split = player_and_pos.rsplit('.')
+        row_data[1] = split[0]
+        row_data.insert(2, split[1][1:])
+        writer.writerow(row_data)
