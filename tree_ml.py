f0025a1a9a68eeca0998c6139a553f3801497272
Layton Webber <webberl@pic.ucla.edu>
2019-03-06 13:44:04 -0800

    Added random forest model

--- tree_ml.py
+++ tree_ml.py
@@ -6,6 +6,7 @@ Created on Tue Mar 05 18:33:53 2019
 """
 
 from sklearn import tree
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
 
 df=pd.read_csv('batter_stats.csv', sep=',',header=0)
@@ -14,12 +15,19 @@ data = []
 target = []
 
 for i in range(len(df)/2):
-    row_data = df[i:i+1].values.tolist()[0][5:22]
+    row_data = df[i:i+1].values.tolist()[0][5:20]
     data.append(row_data[:len(row_data)-1])
     target.append(row_data[len(row_data)-1])
     
     
 clf = tree.DecisionTreeRegressor()
 clf = clf.fit(data, target)
+# Had to fine tune parameters
+regr = RandomForestRegressor(n_estimators = 500, max_depth=4)
+regr = regr.fit(data, target)
 
-print clf.predict([df[1599:1600].values[0][5:19]])
+player = 1400
+
+print clf.predict([df[player:player+1].values.tolist()[0][5:19]])[0]
+print regr.predict([df[player:player+1].values.tolist()[0][5:19]])[0]
+print df[player:player+1].values.tolist()[0][19]
