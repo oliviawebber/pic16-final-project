8490bf72cb62cbbc807287c0730b004c0aeb6009
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2019-03-12 16:02:00 -0700

    Update KNN_pitchers.py

--- KNN_pitchers.py
+++ KNN_pitchers.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
-
-#from sklearn.neighbors import NearestNeighbors
+import matplotlib.pyplot as plt
 from sklearn import neighbors 
 from sklearn.model_selection import train_test_split
 
@@ -16,8 +15,6 @@ targets = targets.astype(int)
 
 df = df.drop(['Rank', 'Player', 'Position', 'Team', 'Season'], axis=1)
 
-#labels = list(df.columns) # header labels
-
 features = np.array(df) # player stats
 
 training_features, test_features, training_targets, test_targets = train_test_split(features, targets, test_size = .4)
@@ -30,6 +27,28 @@ for elem in test_features: # test model
     elem = np.array([elem])
     predictions.append(nbrs.predict(elem))
     
+pred = [] # non numpy array form of predictions
+for elem in predictions:
+    pred.append(elem[0])
+
+era = test_features[:,11] # read test stats into arrays for visualization
+whip = test_features[:,12]
+losses = test_features[:,9]
+so = test_features[:,7]
+wins = test_features[:,8]
+
+plt.xlabel('ERA')
+plt.ylabel('WHIP')
+plt.title('KNN Model Salary Predictions with Hot Color Scheme')
+plt.scatter(era, whip, c=pred, cmap = 'hot')
+plt.show()
+
+plt.xlabel('Strikeouts')
+plt.ylabel('Wins')
+plt.title('KNN Model Salary Predictions with Hot Color Scheme')
+plt.scatter(so, wins, c=pred, cmap = 'hot')
+plt.show()
+
 correct = 0.0
 for i in range(len(predictions)): # assess accuracy of model
     if predictions[i] == test_targets[i]:
