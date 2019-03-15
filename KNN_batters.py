7d8bb228ef37a7c7e01cc94b8ce216589694af85
emmetdonahue <44823177+emmetdonahue@users.noreply.github.com>
2019-03-14 18:45:46 -0700

    Update KNN_batters.py

--- KNN_batters.py
+++ KNN_batters.py
@@ -1,30 +1,23 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  7 15:22:48 2019
-
-@author: EmmetDonahue
-"""
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import neighbors 
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv('batter_stats.csv', sep=',', header=0)
-salaries = np.array(df['Salary'])
+df = pd.read_csv('clean_batter.csv', sep=',', header=0)
+
+salaries = np.array(df['salary'])
 
 sal_range =  np.amax(salaries) - np.amin(salaries)
 sal_interval = sal_range/500
 targets = salaries/sal_interval # target salaries
 targets = targets.astype(int)
 
-df = df.drop(['Rank', 'Player', 'Position', 'Team', 'Season'], axis=1)
+df = df.drop(['num', 'playerID', 'yearID', 'stint', 'teamID', 'IgID', 'prev_salary'], axis=1)
 
 features = np.array(df) # player stats
 
-training_features, test_features, training_targets, test_targets = train_test_split(features, targets, test_size = .4)
+training_features, test_features, training_targets, test_targets = train_test_split(features, targets, test_size = .6)
 
 nbrs = neighbors.KNeighborsClassifier(1)
 nbrs.fit(training_features, training_targets)
